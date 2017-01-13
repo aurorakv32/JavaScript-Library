e3d93c2bdf6060c4cfced50f0b70ebaff7389396
Aurora Wilson <Aurora Wilson>
2017-01-13 18:05:16 -0500

    freeze and document

---
+++ 0.16_document.js
@@ -0,0 +1,24 @@
+// The document is an interface between the webpage and the DOM
+// The document interface describes the common porperties
+// and methods for any kind of document, depending on the type of 
+// document.
+
+// When you call document inside of javaScript, you are pointing it 
+// back to the html document.
+
+// When an HTML document is loaded into a web browser, it becomes a document object.
+
+// The document object is the root node of the HTML document and the "owner" of all other nodes:
+// (element nodes, text nodes, attribute nodes, and comment nodes).
+
+// The document object provides properties and methods to access all node objects, from within JavaScript.
+
+// Tip: The document is a part of the Window object and can be accessed as window.document.
+
+// the document object can be used in several different applications
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementByName()
+// document.getElementsByTagName()
+
+// these to date (1-13-17) are the only ones that I am familiar with.
